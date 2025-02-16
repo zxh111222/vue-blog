e734e94df03a9e5fc9a209ab75ab647be9d82907
zxhhhxz1221 <1305894626@qq.com>
2025-02-16 14:57:18 +0800

    更新单篇的博客内容 PostItem

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,16 +1,21 @@
 <script setup>
-  defineProps({
-    blog: {
-      type: Object,
-      required: true,
-    }
-  })
+defineProps({
+  blog: {
+    type: Object,
+    required: true,
+  }
+})
 </script>
 
 <template>
   <div class="post">
     <h1>{{ blog.title }}</h1>
     <div>{{ blog.content }}</div>
+    <div>
+      <button>删除</button>
+      <button>收藏</button>
+    </div>
+    <hr>
   </div>
 </template>
 
