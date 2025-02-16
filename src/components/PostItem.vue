8252dc2d9f80860db3baddd9480da20bbeba5d80
zxhhhxz1221 <1305894626@qq.com>
2025-02-16 15:25:12 +0800

    博客数据增加 author 和 create_at 字段 相应的更新 PostItem 页面

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -9,16 +9,22 @@ defineProps({
 
 <template>
   <div class="post">
+    <div class="header">
+      <span>作者: {{ blog.author }} 发布于: {{ blog.created_at }}</span>
+      <div>
+        <button>删除</button>
+        <button>收藏</button>
+      </div>
+    </div>
     <h1>{{ blog.title }}</h1>
     <div>{{ blog.content }}</div>
-    <div>
-      <button>删除</button>
-      <button>收藏</button>
-    </div>
     <hr>
   </div>
 </template>
 
 <style scoped>
-
+.header {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,23 @@ let blogs = [
   {
     id: 1,
     title: 'vue 入门',
-    content: '这里讲解了关于 vue 的入门'
+    content: '这里讲解了关于 vue 的入门',
+    author: '小明',
+    created_at: '2025-02-13',
   },
   {
     id: 2,
     title: 'vue 进阶',
-    content: '这里讲解了关于 vue 的进阶'
+    content: '这里讲解了关于 vue 的进阶',
+    author: '中明',
+    created_at: '2025-02-15',
   },
   {
     id: 3,
     title: 'vue 高级',
-    content: '这里讲解了关于 vue 的高级'
+    content: '这里讲解了关于 vue 的高级',
+    author: '大明',
+    created_at: '2025-02-16',
   }
 ]
 </script>
