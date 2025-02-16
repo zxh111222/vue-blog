6df0cbdbce53bc8aec6b79c3ddec4929998ccc89
zxhhhxz1221 <1305894626@qq.com>
2025-02-16 12:32:19 +0800

    在 HomeView 演示多篇博客的功能 - 有 v-for 循环的地方最好加上 :key="blog.id" 提升性能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,28 @@
 <script setup>
-let blog = {
-  title: 'vue 入门',
-  content: '这里讲解了关于 vue 的入门'
-}</script>
+let blogs = [
+  {
+    id: 1,
+    title: 'vue 入门',
+    content: '这里讲解了关于 vue 的入门'
+  },
+  {
+    id: 2,
+    title: 'vue 进阶',
+    content: '这里讲解了关于 vue 的进阶'
+  },
+  {
+    id: 3,
+    title: 'vue 高级',
+    content: '这里讲解了关于 vue 的高级'
+  }
+]
+</script>
 
 <template>
-  <div>
+  <div v-for="blog in blogs" :key="blog.id">
     <h1>{{ blog.title }}</h1>
-    <div>{{ blog.content }}</div>  </div>
+    <div>{{ blog.content }}</div>
+  </div>
 </template>
 
 <style scoped>
