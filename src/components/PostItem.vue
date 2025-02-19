16110e5e918aeaf8116d305b7cc2644422716297
zxhhhxz1221 <1305894626@qq.com>
2025-02-19 19:25:38 +0800

    修改样式

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -23,11 +23,41 @@ defineProps({
 
 <style lang="scss" scoped>
 .header {
+  font-size: 12px;
+  background: #0284c7;
+  padding: 1rem;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-weight: 300;
 
   button {
-    border-radius: 15px;
+    border-radius: 30px;
   }
+
+  div .del {
+    font-size: 20px;
+    padding: 3px;
+    background: #fff;
+    color: #ef4444;
+  }
+
+  div .save {
+    font-size: 20px;
+    padding: 3px;
+    background: #fff;
+    color: #22c55e;
+    margin-left: 10px;
+  }
+}
+
+h1 {
+  padding: 1rem;
+}
+
+p {
+  padding: 0 1rem 1rem;
+  font-weight: 300;
 }
 </style>
--- src/views/PostCreate.vue
+++ src/views/PostCreate.vue
@@ -42,18 +42,14 @@ function sb() {
 form {
   padding: 2rem;
 }
-
 h3 {
   margin-bottom: 1rem;
 }
-
 div {
   margin-bottom: 1rem;
-
   label {
     font-weight: 300;
   }
-
   input,
   textarea {
     max-width: 100%;
@@ -62,24 +58,20 @@ div {
     border: 1px solid #333;
     padding: 5px;
     border-radius: 5px;
-
     &:focus {
       outline: 2px solid #3b82f6;
       border: none;
     }
   }
-
   button {
     background: #3b82f6;
     color: #fff;
     width: 100%;
     padding: 5px;
     border-radius: 5px;
-
     &:hover {
       background: #2563eb;
     }
-
     &:disabled {
       background: #eee;
       cursor: not-allowed
