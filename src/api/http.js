4c628bf9790a987300af8f602267329d6de44266
zxhhhxz1221 <1305894626@qq.com>
2025-02-23 14:28:58 +0800

    axios interceptor - 类比 Java 的 Filter

--- src/api/http.js
+++ src/api/http.js
@@ -5,4 +5,29 @@ const http = axios.create({
   timeout: 5000,
 })
 
+// Request interceptor
+http.interceptors.request.use(
+  config => {
+    console.log('请求：', config)
+    return config
+  },
+  error => {
+    console.error('请求错误：', error)
+    return Promise.reject(error)
+  }
+)
+
+// Response interceptor
+http.interceptors.response.use(
+  response => {
+    console.log('响应：', response)
+    //return response.data
+    return response
+  },
+  error => {
+    console.log('响应错误：', error)
+    return Promise.reject(error)
+  }
+)
+
 export default http
