57af797fb152da1094fd086cf1e8fb826686f196
zxhhhxz1221 <1305894626@qq.com>
2025-02-23 10:15:00 +0800

    复习 flex 布局...
    - display: flex;
    - justify-content: space-between;

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import { usePostStore } from "@/stores/posts.js";
+import {RouterView} from 'vue-router'
+import {usePostStore} from "@/stores/posts.js";
 
 let postsStore = usePostStore();
 
@@ -10,14 +10,18 @@ postsStore.getPosts()
 <template>
   <header>
     <nav>
-      <RouterLink to="/" class="nav-link">首页</RouterLink>
-      <RouterLink to="/post-create" class="nav-link">创建博客</RouterLink>
-      <RouterLink to="/about" class="nav-link">关于</RouterLink>
-      <RouterLink to="/resume" class="nav-link">简历</RouterLink>
+      <div>
+        <RouterLink to="/" class="nav-link">首页</RouterLink>
+        <RouterLink to="/post-create" class="nav-link">创建博客</RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/about" class="nav-link">关于</RouterLink>
+        <RouterLink to="/resume" class="nav-link">简历</RouterLink>
+      </div>
     </nav>
   </header>
 
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style lang="scss" scoped>
@@ -25,11 +29,15 @@ nav {
   padding: 1rem 2rem;
   background: #1e40af;
   border-bottom: 1px solid #60a5fa;
+  display: flex;
+  justify-content: space-between;
+
   .nav-link {
     color: #fff;
     padding: 5px 15px;
     transition: all 0.2s ease-in-out;
     margin-right: 1rem;
+
     &:hover {
       background: #60a5fa;
     }
