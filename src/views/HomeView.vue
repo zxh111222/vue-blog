cb012260561922af31f1b2ed81fdf39bd3b8ff2a
zxhhhxz1221 <1305894626@qq.com>
2025-02-23 11:54:21 +0800

    在需要请求后端接口的地方，引入 post.js,同时演示不使用store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,28 @@
 <script setup>
 import PostItem from "@/components/PostItem.vue";
 import MyWrapper from "@/components/MyWrapper.vue";
-import { usePostStore } from "@/stores/posts.js";
+import { getPosts } from "@/api/post.js";
+import { onMounted, ref } from "vue";
 
-let postsStore = usePostStore();
+const postList = ref([])
+
+function getPostList(){
+  getPosts()
+    .then((res) => {
+      postList.value = res.data.posts
+    })
+    .catch(error => console.log(error))
+}
+
+// 在组件加载时会调用这个方法
+onMounted(() => {
+  getPostList()
+})
 </script>
 
 <template>
 
-  <div v-for="blog in postsStore.sortedPosts" :key="blog.id">
+  <div v-for="blog in postList" :key="blog.id">
     <MyWrapper>
       <PostItem :blog='blog' />
     </MyWrapper>
