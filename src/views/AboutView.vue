937033982f3cbe0172fee0f3c7ed5a419121f0cc
zxhhhxz1221 <1305894626@qq.com>
2025-02-23 10:42:05 +0800

    使用别人写好的组件 - element-plus - 自己去找感兴趣的组件加入组件 再引入分割线 divider

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -29,6 +29,8 @@ const tableData = [
 
     <el-button type="primary">Primary</el-button>
 
+    <el-divider content-position="left">Rabindranath Tagore</el-divider>
+
     <el-table :data="tableData" style="width: 100%">
       <el-table-column prop="date" label="Date" width="180" />
       <el-table-column prop="name" label="Name" width="180" />
